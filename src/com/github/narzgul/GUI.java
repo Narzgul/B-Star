d90b025b981be14e0c7c1318850b3080d89fad3a
T-Zocker <tzoockeer@gmail.com>
2022-04-24 13:13:31 +0200

    Synchronization with main thread

--- src/com/github/narzgul/GUI.java
+++ src/com/github/narzgul/GUI.java
@@ -2,17 +2,18 @@ package src.com.github.narzgul;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class GUI {
-    JFrame frame;
-    JPanel panel;
-    JButton[][] buttons;
-    ArrayList<int[]> obstacle = new ArrayList<>();
+    private JFrame frame;
+    private JPanel grid;
+    private JButton[][] buttons;
+    private Object notifier;
+
+    private ArrayList<int[]> obstacle = new ArrayList<>();
     public GUI(int length, int height) {
+        notifier = new Object();
+
         frame = new JFrame();
         frame.setSize(500, 500);
         frame.setTitle("A*");
@@ -20,9 +21,20 @@ public class GUI {
         frame.setVisible(true);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // Stop process on closing
 
-        panel = new JPanel();
-        panel.setLayout(new GridLayout(length,height));
-        frame.add(panel);
+        JPanel borderLayout = new JPanel();
+        borderLayout.setLayout(new BorderLayout());
+        grid = new JPanel();
+        grid.setLayout(new GridLayout(length,height));
+        borderLayout.add(grid, BorderLayout.CENTER); // Middle of the panel
+        JPanel statusBar = new JPanel();
+        statusBar.setLayout(new FlowLayout());
+        borderLayout.add(statusBar, BorderLayout.SOUTH); // Bottom of the panel
+        frame.add(borderLayout);
+
+        JTextField status = new JTextField("Erik ist gay");
+        status.setBorder(null); // Remove border and background
+        status.setBackground(null);
+        statusBar.add(status);
 
         buttons = new JButton[length][height];
         for (int i = 0; i < length; i++) {
@@ -32,14 +44,25 @@ public class GUI {
                 int finalI = i; // Final vars for the ActionListener
                 int finalJ = j;
                 buttons[i][j].addActionListener(clickEvent -> {
-                    if (clickEvent.getSource() instanceof JButton button) {
-                        button.setBackground(Color.GRAY);
-                        System.out.println("Cords: " + finalI + ',' + finalJ);
-                        obstacle.add(new int[]{finalI, finalJ}); // Add cords to list
+                    synchronized (notifier) {
+                        if (clickEvent.getSource() instanceof JButton button) {
+                            button.setBackground(Color.GRAY);
+                            System.out.println("Cords: " + finalI + ',' + finalJ);
+                            obstacle.add(new int[]{finalI, finalJ}); // Add cords to list
+                            notifier.notify();
+                        }
                     }
                 });
-                panel.add(buttons[i][j]);
+                grid.add(buttons[i][j]);
             }
         }
+    }
+
+    public ArrayList<int[]> getObstacle() {
+        return obstacle;
+    }
+
+    public void setNotifier(Object notifier) {
+        this.notifier = notifier;
     }
 }
