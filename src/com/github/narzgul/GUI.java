b77f6929d2abd3ac6fe561ee69f7e5916413e3e8
T-Zocker <tzoockeer@gmail.com>
2022-04-24 02:40:01 +0200

    Cords in ArrayList

--- src/com/github/narzgul/GUI.java
+++ src/com/github/narzgul/GUI.java
@@ -4,19 +4,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class GUI {
     JFrame frame;
     JPanel panel;
     JButton[][] buttons;
-    boolean testflag;
+    ArrayList<int[]> obstacle = new ArrayList<>();
     public GUI(int length, int height) {
         frame = new JFrame();
         frame.setSize(500, 500);
         frame.setTitle("A*");
-        frame.setLocationRelativeTo(null);
+        frame.setLocationRelativeTo(null); // Middle of the screen
         frame.setVisible(true);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // Stop process on closing
 
         panel = new JPanel();
         panel.setLayout(new GridLayout(length,height));
@@ -27,12 +29,13 @@ public class GUI {
             for (int j = 0; j < height; j++) {
                 buttons[i][j] = new JButton();
                 buttons[i][j].setBackground(Color.WHITE);
-                int finalI = i;
+                int finalI = i; // Final vars for the ActionListener
                 int finalJ = j;
                 buttons[i][j].addActionListener(clickEvent -> {
                     if (clickEvent.getSource() instanceof JButton button) {
                         button.setBackground(Color.GRAY);
                         System.out.println("Cords: " + finalI + ',' + finalJ);
+                        obstacle.add(new int[]{finalI, finalJ}); // Add cords to list
                     }
                 });
                 panel.add(buttons[i][j]);
