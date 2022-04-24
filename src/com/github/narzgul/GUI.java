71a838e58f73f5f6ace2bc94fbbbb87d7d30c02d
T-Zocker <tzoockeer@gmail.com>
2022-04-24 02:30:04 +0200

    Cords within ActionListener

--- src/com/github/narzgul/GUI.java
+++ src/com/github/narzgul/GUI.java
@@ -2,11 +2,14 @@ package src.com.github.narzgul;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUI {
     JFrame frame;
     JPanel panel;
     JButton[][] buttons;
+    boolean testflag;
     public GUI(int length, int height) {
         frame = new JFrame();
         frame.setSize(500, 500);
@@ -24,9 +27,12 @@ public class GUI {
             for (int j = 0; j < height; j++) {
                 buttons[i][j] = new JButton();
                 buttons[i][j].setBackground(Color.WHITE);
+                int finalI = i;
+                int finalJ = j;
                 buttons[i][j].addActionListener(clickEvent -> {
                     if (clickEvent.getSource() instanceof JButton button) {
                         button.setBackground(Color.GRAY);
+                        System.out.println("Cords: " + finalI + ',' + finalJ);
                     }
                 });
                 panel.add(buttons[i][j]);
